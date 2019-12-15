44ca9838f21cb69c952e88ccea7e2a1a84ecff3c
checkfelix123 <42866735+checkfelix123@users.noreply.github.com>
2019-12-15 23:22:48 +0100

    updating project and practising forms

---
+++ 8_angular-forms/src/app/td-practise/td-practise.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-td-practise',
+  templateUrl: './td-practise.component.html',
+  styleUrls: ['./td-practise.component.css']
+})
+export class TdPractiseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
